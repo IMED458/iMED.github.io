5fc421287c563f65b82e4b100dbdd9dfd0aec7fd
IMED458 <imedashvili700@gmail.com>
2026-05-21 00:00:03 +0400

    Fix Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/gbmnsubnmit/',
+    base: '/gbmnsubmit.github.io/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
